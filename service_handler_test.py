8bbee6ef5091ec4d45637de514a66798853d646a
CDP <kevinchen345@gmail.com>
2022-07-25 02:09:27 +1000

    implemented function to issue assets

--- service_handler_test.py
+++ service_handler_test.py
@@ -1,7 +1,7 @@
 import test
 import requests
-from stellar_sdk import Asset
-from stellar_sdk import Keypair
+from stellar_sdk import Asset, Keypair, Network, Server, TransactionBuilder
+
 
 
 """
@@ -25,7 +25,7 @@ Tasks:
 5. Finalization/ closure
 """
 # Generate keypair (public and secret) https://stellar-sdk.readthedocs.io/en/stable/generate_keypair.html
-# Required for interacting with Horizon node
+# Required for creating account
 # Can be generated from a given public or secret key, in this case we get a random one
 
 def generateKeypair():
@@ -41,9 +41,6 @@ def friendbotAccount(keypair):
     response = requests.get(url, params={"addr": keypair.public_key})
     return response
 
-
-
-
 # Generate asset from Stellar SDK
 def generateAsset(keypair):
     # Creates TEST asset issued by keypair.public_key
@@ -51,6 +48,69 @@ def generateAsset(keypair):
     is_native = test_asset.is_native()  # False
     return test_asset
 
+
+# Issuing an asset. Inherently creates a trustline
+# Assets are only created when they are first transacted
+def issueAsset(issuing_asset, issuing_secret_key, distributing_secret_key):
+
+    # Configure Stellar SDK to talk to the horizon instance hosted by Stellar.org
+    # To use the live network, set the hostname to 'https://horizon.stellar.org'
+    server = Server(horizon_url="https://horizon-testnet.stellar.org")
+    # Use test network, if you need to use public network, please set it to `Network.PUBLIC_NETWORK_PASSPHRASE`
+    network_passphrase = Network.TESTNET_NETWORK_PASSPHRASE
+
+    # Keys for accounts to issue and receive the new asset
+    issuing_keypair = Keypair.from_secret(
+        issuing_secret_key
+    )
+    issuing_public = issuing_keypair.public_key
+
+    distributor_keypair = Keypair.from_secret(
+        distributing_secret_key
+    )
+    distributor_public = distributor_keypair.public_key
+
+    # Transactions require a valid sequence number that is specific to this account.
+    # We can fetch the current sequence number for the source account from Horizon.
+    distributor_account = server.load_account(distributor_public)
+
+    # First, the receiving account must trust the asset
+    trust_transaction = (
+        TransactionBuilder(
+            source_account=distributor_account,
+            network_passphrase=network_passphrase,
+            base_fee=100,
+        )
+        #  The `changeTrust` operation creates (or alters) a trustline
+        #  The `limit` parameter below is optional
+        .append_change_trust_op(asset=issuing_asset, limit="1000")
+        .set_timeout(100)
+        .build()
+    )
+
+    trust_transaction.sign(distributor_keypair)
+    trust_transaction_resp = server.submit_transaction(trust_transaction)
+    print(f"Change Trust Transaction Resp:\n{trust_transaction_resp}")
+
+    issuing_account = server.load_account(issuing_public)
+    # Second, the issuing account actually sends a payment using the asset.
+    payment_transaction = (
+        TransactionBuilder(
+            source_account=issuing_account,
+            network_passphrase=network_passphrase,
+            base_fee=100,
+        )
+        .append_payment_op(
+            destination=distributor_public,
+            asset=issuing_asset,
+            amount="1",
+        )
+        .build()
+    )
+    payment_transaction.sign(issuing_keypair)
+    payment_transaction_resp = server.submit_transaction(payment_transaction)
+    print(f"Payment Transaction Resp:\n{payment_transaction_resp}")
+
 keypair = generateKeypair()
 print(keypair.public_key)
-print(friendbotAccount(keypair))
+print(friendbotAccount(keypair).json)

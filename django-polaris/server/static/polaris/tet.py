18d0d926e4ef0efa4cb98e7729875470279e520f
CDP <kevinchen345@gmail.com>
2022-11-01 03:38:26 +1100

    removed test file

--- django-polaris/server/static/polaris/tet.py
+++
@@ -1,9 +0,0 @@
-import toml
-
-#toml_dict = toml.loads(toml_string)  # Read from a string
-
-input_file_name = "local-stellar.toml"
-with open(input_file_name) as toml_file:
-    toml_dict = toml.load(toml_file)
-
-print(toml_dict)
